45354f8be691b6aef5a29b2580bfbdf0596f31d1
khanasif1 <khanasif1@gmail.com>
2021-09-26 22:41:43 +1000

    http FnApp to access AAD using graphAPI

---
+++ _code/power.microsoft.graph/sample.aad.usergroup.http.fnApp/AuthenticationConfig.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace sample.aad.usergroup.http.fnApp
+{
+    /// <summary>
+    /// Description of the configuration of an AzureAD public client application (desktop/mobile application). This should
+    /// match the application registration done in the Azure portal
+    /// </summary>
+    public class AuthenticationConfig
+    {
+        /// <summary>
+        /// instance of Azure AD, for example public Azure or a Sovereign cloud (Azure China, Germany, US government, etc ...)
+        /// </summary>
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+       
+        /// <summary>
+        /// Graph API endpoint, could be public Azure (default) or a Sovereign cloud (US government, etc ...)
+        /// </summary>
+        public string ApiUrl { get; set; } = "https://graph.microsoft.com/";
+
+        /// <summary>
+        /// The Tenant is:
+        /// - either the tenant ID of the Azure AD tenant in which this application is registered (a guid)
+        /// or a domain name associated with the tenant
+        /// - or 'organizations' (for a multi-tenant application)
+        /// </summary>
+        public string Tenant { get; set; }
+
+        /// <summary>
+        /// Guid used by the application to uniquely identify itself to Azure AD
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// AAD group ID
+        /// </summary>
+        public string AADGroupId { get; set; }
+
+        /// <summary>
+        /// URL of the authority
+        /// </summary>
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, Tenant);
+            }
+        }
+
+        /// <summary>
+        /// Client secret (application password)
+        /// </summary>
+        /// <remarks>Daemon applications can authenticate with AAD through two mechanisms: ClientSecret
+        /// (which is a kind of application password: this property)
+        /// or a certificate previously shared with AzureAD during the application registration 
+        /// (and identified by the CertificateName property belows)
+        /// <remarks> 
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Name of a certificate in the user certificate store
+        /// </summary>
+        /// <remarks>Daemon applications can authenticate with AAD through two mechanisms: ClientSecret
+        /// (which is a kind of application password: the property above)
+        /// or a certificate previously shared with AzureAD during the application registration 
+        /// (and identified by this CertificateName property)
+        /// <remarks> 
+        public string CertificateName { get; set; }
+
+        /// <summary>
+        /// Service Bus connection string and queue name
+        /// </summary>
+        public string SBconnectionString { get; set; }
+        public string SBQueueName { get; set; }
+
+
+        /// <summary>
+        /// Reads the configuration from a json file
+        /// </summary>
+        /// <param name="path">Path to the configuration json file</param>
+        /// <returns>AuthenticationConfig read from the json file</returns>        
+        public AuthenticationConfig()
+        {
+            //IConfigurationRoot Configuration;
+
+            //var builder = new ConfigurationBuilder()
+            // .SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile(path);
+
+            //Configuration = builder.Build();
+            //return Configuration.Get<AuthenticationConfig>();
+            
+            this.ClientId = System.Environment.GetEnvironmentVariable("Dest_ClientId", EnvironmentVariableTarget.Process);
+            this.ClientSecret = System.Environment.GetEnvironmentVariable("Dest_ClientSecret", EnvironmentVariableTarget.Process);
+            this.Tenant = System.Environment.GetEnvironmentVariable("Dest_Tenant", EnvironmentVariableTarget.Process);
+
+            this.SBconnectionString = System.Environment.GetEnvironmentVariable("AADSYNC_SERVICEBUS", EnvironmentVariableTarget.Process);
+            this.SBQueueName = System.Environment.GetEnvironmentVariable("invitequeue", EnvironmentVariableTarget.Process);            
+
+        }
+    }
+
+
+
+}
+
---
+++ _code/power.microsoft.graph/sample.aad.usergroup.http.fnApp/fnAccessAadUserGroup.cs
@@ -0,0 +1,99 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace sample.aad.usergroup.http.fnApp
+{
+    public static class fnAccessAadUser
+    {
+        [FunctionName("fnAccessAadUser")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            
+
+            string groupResponse = string.Empty;            
+            try
+            {
+                JObject _graphresponse = await GetAADUserAsync(log);
+                groupResponse = _graphresponse.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                log.LogInformation($" Error: {ex.Message}");
+            }
+
+            return new OkObjectResult(groupResponse.ToString());
+        }
+        private static async Task<JObject> GetAADUserAsync(ILogger log)
+        {
+            try
+            {
+
+                AuthenticationConfig config = new AuthenticationConfig();
+                // Even if this is a console application here, a daemon application is a confidential client application
+                IConfidentialClientApplication app;
+
+                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                    .WithClientSecret(config.ClientSecret)
+                    .WithAuthority(new Uri(config.Authority))
+                    .Build();
+
+                // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+                // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+                // a tenant administrator. 
+                string[] scopes = new string[] { $"{config.ApiUrl}.default" };
+
+                JObject _destinationUseresult = null;
+
+                AuthenticationResult result = null;
+                try
+                {
+                    result = await app.AcquireTokenForClient(scopes)
+                        .ExecuteAsync();
+                }
+                catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+                {
+                    // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                    // Mitigation: change the scope to be as expected
+                    log.LogInformation($" Scope provided is not supported for destination AAD instance");
+
+                }
+               
+                if (result != null)
+                {
+                    var httpClient = new HttpClient();
+                    var apiCaller = new ProtectedApiCallHelper(httpClient);
+                    //Get all users from the destination AAD group for comparison with User with the AAD Source
+                    _destinationUseresult = await apiCaller.CallWebApiAndProcessResultASync(
+                                                               $"{config.ApiUrl}v1.0/users",                                                               
+                                                               result.AccessToken,
+                                                               log);
+                   
+                    log.LogInformation($" Graph API resuqest completed. ");
+                }
+                return _destinationUseresult;
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, $" **ERROR IN SyncAADUserAsync***{ex.Message}");
+                throw ex;
+            }
+        }
+    }
+}
